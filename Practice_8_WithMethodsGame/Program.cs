8f0bf931caf9e97a0e9ef0f55aab5ddd3ad331a4
GOOGLE228 <75929954+GOOGLE228@users.noreply.github.com>
2025-03-02 21:41:44 +0300

    Сохранил HP через static, сделал ловушку

--- Practice_8_WithMethodsGame/Program.cs
+++ Practice_8_WithMethodsGame/Program.cs
@@ -9,61 +9,66 @@ namespace Practice_8_WithMethodsGame
     internal class Program
     {
         static Random random = new Random();
+        static int pHP = 100;
         static void Main(string[] args)
         {
-            //int roomNumber = 0;
+            int roomNumber = 0;
 
-            //InitializeGame();
             StartGame();
-            ProcessRoom();
+            ProcessRoom(roomNumber);
 
 
             Console.ReadKey();
-            
+
         }
-        public static int InitializeGame()
+        public static int PlayerHP()
         {
             int pHP = 100;
-            int maxHP = 100;
-            int gold = 10;
-            int potion = 2;
-            int arrows = 5;
-            Dictionary<string, int> weapon = new Dictionary<string, int>()
-            {
-                { "меч", random.Next(10, 21) },
-                { "лук", random.Next(5, 16) }
-            };
             return pHP;
+        }
+        public static int MaxPlayerHP()
+        {
+            int maxHP = 100;
+            return maxHP;
+        }
+        public static int Gold()
+        {
+            int gold = 10;
+            return gold;
+        }
+        public static int Potion()
+        {
+            int potion = 2;
+            return potion;
+        }
+        public static int Arrows()
+        {
+            int arrows = 5;
+            return arrows;
         }
         public static void StartGame()
         {
             Console.WriteLine("Добро пожаловать странник!");
         }
-        public static void ProcessRoom()
+        public static void ProcessRoom(int roomNumber)
         {
-            int pHP = InitializeGame();
             int monsterHP = 0;
             int monsterAttack = 0;
             Console.WriteLine("Странник попадает в комнату с...");
             string[] dungeonMap = { "Монстр", "Ловушка", "Обычный сундук", "Проклятый сундук", "Торговец", "Алтарь усиления", "Темный маг", "Событие", "Финальная комната" };
             for (int i = 0; i < 15; i++)
             {
-                int roomNumber = random.Next(0, dungeonMap.Length);
+                roomNumber = random.Next(0, 2); // dungeonMap.Length
                 string room = dungeonMap[roomNumber];
-                switch(room)
+                switch (room)
                 {
                     case "Монстр":
                         FightMonster(monsterHP, monsterAttack);
-                        //pHP = FightMonster(monsterHP, monsterAttack);
                         break;
                     case "Ловушка":
-                        Console.WriteLine("\nС ловушкой!");
-                        pHP = Trap();
-                        Console.WriteLine($"У играка {pHP} HP");
+                        Trap();
                         break;
                     case "Обычный сундук":
-                        Console.WriteLine("\nС сундуком!");
-
                         break;
                     case "Проклятый сундук":
                         break;
@@ -76,22 +81,22 @@ namespace Practice_8_WithMethodsGame
                     case "Событие":
                         break;
                 }
-                //maxHP = pHP;
             }
         }
         public static int FightMonster(int monsterHP, int monsterAttack)
         {
             monsterHP = random.Next(20, 51);
-            int pHP = InitializeGame();
-            int arrows = 5;
+            //int pHP = PlayerHP();
+            int arrows = Arrows();
             Dictionary<string, int> weapon = new Dictionary<string, int>()
-            {
-                { "меч", random.Next(10, 21) },
-                { "лук", random.Next(5, 16) }
-            };
+    {
+        { "меч", random.Next(10, 21) },
+        { "лук", random.Next(5, 16) }
+    };
             Console.WriteLine("\nС монстром!");
             while (monsterHP > 0 && pHP > 0)
             {
+                Console.WriteLine($"\nУ играка {pHP} HP");
                 Console.WriteLine($"У монстра {monsterHP} HP");
                 Console.Write("Выберите оружие: 'меч' или 'лук': ");
                 var chooseWeapon = Console.ReadLine();
@@ -117,7 +122,6 @@ namespace Practice_8_WithMethodsGame
                 }
                 else Console.WriteLine("Такого оружия у вас нет!");
                 Console.WriteLine($"Монстр наносит ответный удар: {monsterAttack}");
-                Console.WriteLine($"\nУ играка {pHP} HP");
             }
             if (pHP > 0)
                 Console.WriteLine("Победа! Монстр повержен!");
@@ -128,50 +132,25 @@ namespace Practice_8_WithMethodsGame
             }
             return pHP;
         }
-        public static int Trap()
+        public static void Trap()
         {
-            int pHP = InitializeGame();
-            pHP -= random.Next(5, 21);
-            return pHP;
-        }
-        public static void Chest()
-        {
-            string answer1 = "4";
-            bool corranswer = true;
-            Console.WriteLine("Чтобы открыть сундук, игрок должен решить математическую загадку: 2 + 2");
-            while (corranswer)
+            int monsterHP = 0;
+            int monsterAttack = 0;
+            //int pHP = FightMonster(monsterHP, monsterAttack);
+            Console.WriteLine("\nС ловушкой!");
+            int fall = random.Next(2);
+            if (fall == 1)
             {
-                Console.Write("Ваш ответ: ");
-                var answer = Console.ReadLine();
-                if (answer == answer1)
-                {
-                    Console.WriteLine("Аааааайй, ТИГР! Лови лут!");
-                    int value = random.Next(0, loot.Length - 1);
-                    string item = loot[value];
-                    if (item == "Зелье")
-                    {
-                        Console.WriteLine(inventory.Length);
-                        Console.WriteLine(potion);
-                        if (inventory.Length <= 0)
-                        {
-                            Console.WriteLine("Ваш инвентарь переполнен!");
-                        }
-                        else
-                        {
-                            inventory[potion] = 1;
-                            potion--;
-                        }
-                    }
-                    else if (item == "Золото")
-                    {
-                        golds += random.Next(10, 26);
-                    }
-                    else arrows += 3;
-                    corranswer = false;
-                }
-                else Console.WriteLine("Неправильно, попробуй ещё раз!");
+                Console.WriteLine("Вы попали в ловушку!");
+                pHP -= random.Next(5, 21);
             }
-            Console.WriteLine($"Зелий: {potion}\nЗолото: {golds}\nСтрелы: {arrows}");
+            else Console.WriteLine("Фух! Ловушка давно устарела и не работает.");
+            Console.WriteLine($"У играка {pHP} HP");
+            if (pHP < 0)
+            {
+                Console.WriteLine("Вы погибли.");
+                int i = 100;
+            }
         }
     }
 }
